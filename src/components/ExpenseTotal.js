6e01704231bcadae2931ee1ca91e4fce74447541
Keematics <agbajeabdulakeems@gmail.com>
2023-03-01 13:17:51 -0500

    Try to update the spent amount in the UI

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -2,14 +2,16 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseTotal = () => {
-    const { currency } = useContext(AppContext); // I removed "expenses" from the desctructuring
-    // const totalExpenses = expenses.reduce((total, item) => {
-    //     return (total += (item.unitprice * item.quantity));
-    // }, 0);
+    const { expenses, currency, budget } = useContext(AppContext); // I removed "expenses" from the desctructuring
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += (item.unitprice * item.quantity));
+    }, 0);
 
+    const amount_spent = parseInt(budget - totalExpenses);
+    console.log(typeof(amount_spent));
     return (
         <div className='alert alert-primary'>
-            <span>Spent so far {currency} 940</span>
+            <span>Spent so far {currency} {amount_spent}</span>
         </div>
     );
 };
