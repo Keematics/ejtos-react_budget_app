44ef90d50ab3e573524cb000d8e33f62d5ff6285
Keematics <agbajeabdulakeems@gmail.com>
2023-03-01 03:38:39 -0500

    Add allocation table in the expenseList component with the expenseItem in it, import and update the App.js with expenseList and Allocation components using bootstrap for styling, install the required dependecies

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
-import Budget from './components/Budget';
+// import Budget from './components/Budget';
 
 // Add code to import the other components here under
+import AllocationForm from "./components/AllocationForm";
+import ExpenseList from "./components/ExpenseList";
 
 
 import { AppProvider } from './context/AppContext';
@@ -15,18 +17,25 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
+                    {/* <Budget />         */}
 
                         {/* Add Remaining component here under */}        
 
-                        {/* Add ExpenseTotal component here under */}        
+                     {/* Add ExpenseTotal component here under */}        
                        
-                        {/* Add ExpenseList component here under */}         
+                    <h3 className='mt-3'>Allocation</h3>
+                    <div className='row '>
+                        <div className='col-sm'>
+                            <ExpenseList />
+                        </div>
+                    </div>         
 
-                        {/* Add ExpenseItem component here under */}        
-
-                        {/* Add AllocationForm component here under */}        
-
+                    <h3 className='mt-3'>Change Allocation</h3>
+                        <div className='row mt-3'>
+                            <div className='col-sm'>
+                                <AllocationForm />
+                            </div>
+                        </div>
                 </div>
             </div>
         </AppProvider>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,38 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+import { FaTimesCircle} from 'react-icons/fa';
+import {FcPlus, FcMinus } from "react-icons/fc"
 
+const ExpenseItem = (props) => {
+    const { dispatch, } = useContext(AppContext);
+
+    const handleAdd = () => {
+        
+    }
+    const handleSubtract = () => {
+
+    }
+    const handleDeleteItem = () => {
+        const item = {
+            name: props.name,
+        };
+
+        dispatch({
+            type: 'DELETE_ITEM',
+            payload: item,
+        });
+    };
+
+
+    return (
+        <tr>
+        <td>{props.name}</td>
+        <td>{props.cost}</td>
+        <td><FcPlus size="2.2em" color="green" onClick={handleAdd} ></FcPlus></td>
+        <td><FcMinus size="2.2em" color="red" onClick={handleSubtract} ></FcMinus></td>
+        <td><FaTimesCircle size='1.5em' color="grey" onClick={handleDeleteItem}></FaTimesCircle></td>
+        </tr>
+    );
+};
+
+export default ExpenseItem;
